dcbe46bd6328a2911673ffa34eba1791b296a138
lambtor <lambtor@yahoo.com>
2023-03-03 13:33:02 -0600

    Update code.py
    severe refactor in progress for changed scope of project

--- code.py
+++ code.py
@@ -1,70 +1,50 @@
 import time
 import board
 import digitalio
-from audiomp3 import MP3Decoder
+import rotaryio
 from ltp305 import ltp305
-import os
-
-try:
-    from audioio import AudioOut
-except ImportError:
-    try:
-        from audiopwmio import PWMAudioOut as AudioOut
-    except ImportError:
-        pass  # not always supported by every board!
+from font import font
 
 # timeout is in seconds
-TIMEOUT = 0.25
+TIMEOUT = 0.015
 mnLastPoll = 0
-mnSongPoll = 0
 mnBrightness = 0
 mnDispVal = 0
 mbBrightAsc = True
 mbRightDec = True
 mbLeftDec = False
-# moMatrix = ltp305(sda=board.GP16, scl=board.GP17, i2cAddress=0x61)
-moMatrix = ltp305(sda=board.GP16, scl=board.GP17, i2cAddress=0x63)
+mpEncodeA = board.GP7
+mpEncodeB = board.GP8
+
+btnCENTER = digitalio.DigitalInOut(board.GP9)
+btnUP = digitalio.DigitalInOut(board.GP12)
+btnDOWN = digitalio.DigitalInOut(board.GP10)
+btnRIGHT = digitalio.DigitalInOut(board.GP11)
+btnLEFT = digitalio.DigitalInOut(board.GP13)
+
+btnUP.switch_to_input(digitalio.Pull.UP)
+btnCENTER.switch_to_input(digitalio.Pull.UP)
+btnDOWN.switch_to_input(digitalio.Pull.UP)
+btnRIGHT.switch_to_input(digitalio.Pull.UP)
+btnLEFT.switch_to_input(digitalio.Pull.UP)
+
+moRotary = rotaryio.IncrementalEncoder(mpEncodeA, mpEncodeB)
+# stemma connector is i2c0 -> GP4, SCL -> GP5
+# need to refactor this to be a list of 2 pairs of matrices
+# also need to change this to use i2c1 instead of i2c0
+# i2c1 -> GP18, GP19
+moMatrix = ltp305(sda=board.GP16, scl=board.GP17, i2cAddress=0x61)
 moMatrix.clear()
-moMatrix.brightness(48)
-# moMatrix.writeChar("l", "a", True)
-# moMatrix.writeChar("r", "b", False)
-# moMatrix.update()
-# moMatrix.setDecimal(True, False)
-# msTestString = " It's FIDGET TIME dawg (cat)! "
-msTestString = list(filter(lambda x: x.lower().endswith("mp3"), os.listdir("/")))[0].replace(".mp3", "")
-mnScrollIndex = 0
-# moLeftPin = digitalio.DigitalInOut(board.GP14)
-# moLeftPin.switch_to_output(value=True)
-# moRightPin = digitalio.DigitalInOut(board.GP13)
-# moRightPin.switch_to_output(value=True)
+moMatrix.brightness(20)
+# i2c0 is for stemma - fm module
+# GP29 on pico lipo is for battery voltage
+msTemp = font.get("a")
 
-moAudioPlaying = False
-mp3Filename = open(list(filter(lambda x: x.lower().endswith("mp3"), os.listdir("/")))[0], "rb")
-moDecoder = MP3Decoder(mp3Filename)
-moDecoder.file = open(list(filter(lambda x: x.lower().endswith("mp3"), os.listdir("/")))[0], "rb")
-# right is red - pins connected with alligator clips right now
-# right_channel=board.GP13, 
-moAudio = AudioOut(board.GP28, quiescent_value=-32768)
-
-while True:
-    if ((time.monotonic() - mnLastPoll) > TIMEOUT):
-        # scroll string
-        mnLastPoll = time.monotonic()
-        moMatrix.writeSubstring([char1 for char1 in msTestString], mnScrollIndex)
-        moMatrix.update()
-        mnScrollIndex += 1
-        
-        if (mnScrollIndex >= (len(msTestString) + 1)):
-            mnScrollIndex = 0
-        if (moAudioPlaying == False and (time.monotonic() - mnSongPoll) > 10):            
-            moAudioPlaying = True
-            print("started playing " + str(msTestString))
-            # moAudio.play(moDecoder)
-            # while moAudio.playing:
-             # pass
-        """
+while False:
+    # scrolling display
+    if (time.monotonic() - mnLastPoll) > TIMEOUT:
         lstVal = [int(a) for a in str(mnDispVal)]
-        if (len(lstVal) == 1):
+        if len(lstVal) == 1:
             moMatrix.writeChar("r", lstVal[0], mbRightDec)
             moMatrix.writeChar("l", 0, mbRightDec)
         else:
@@ -76,7 +56,5 @@ while True:
         moMatrix.update()
         mnLastPoll = time.monotonic()
         mnDispVal += 1
-        if (mnDispVal > 99):
+        if mnDispVal > 99:
             mnDispVal = 0
-        """
-
